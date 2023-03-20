0114516e767c1930f257e863aec9737ffa1cab36
manish04042003 <manishkumar817835@gmail.com>
2023-03-21 00:56:25 +0530

    Testimonial Section Start

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-// javaScript for Faqs sections 
-const accordionItems = document.querySelectorAll('.accordion-item');
+// javaScript for Faqs sections
+const accordionItems = document.querySelectorAll(".accordion-item");
 
-accordionItems.forEach(item => {
-  const button = item.querySelector('.accordion-button');
-  const content = item.querySelector('.accordion-content');
-  
-  button.addEventListener('click', () => {
-    item.classList.toggle('active');
-    content.style.maxHeight = item.classList.contains('active') ? content.scrollHeight + 'px' : 0;
+accordionItems.forEach((item) => {
+  const button = item.querySelector(".accordion-button");
+  const content = item.querySelector(".accordion-content");
+
+  button.addEventListener("click", () => {
+    item.classList.toggle("active");
+    content.style.maxHeight = item.classList.contains("active")
+      ? content.scrollHeight + "px"
+      : 0;
   });
 });
-
-
-
-
-
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -37,8 +34,60 @@ span.onclick = function () {
   modal.style.display = "none";
 };
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
+
+// testimonial
+
+
+
+$(document).on('ready', function () {
+  $(".regular").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+  });
+});
+
+
+$(document).on("ready", function () {
+  $(".regular").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
