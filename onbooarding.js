965d80fdb216f9c4ef51a1d872eda0b78fc5c8e0
manish04042003 <manishkumar817835@gmail.com>
2023-04-21 19:28:25 +0530

    image link change all done

--- onbooarding.js
+++ onbooarding.js
@@ -27,7 +27,7 @@ for(let i=0 ; i<nextBtn.length ; i++){
 // });
 
 document.querySelector('.done-btn').addEventListener('click', function() {
-  party.confetti(document.querySelector('.modal-content-Con'), {
+  party.confetti(document.querySelector('body'), {
     /**
      * Whether the debugging mode should be enabled.
      */
